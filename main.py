2d95aa76bcde44f1d31bf252f560638d974f6835
Alexander Walker <alexanderwalker00@hotmail.com>
2023-01-15 13:31:19 +0000

    functioning powerups, and slightly more advanced AI

--- main.py
+++ main.py
@@ -70,6 +70,70 @@ def waveTransition(player_x, player_y, wave, tile_size, bg, screen, clock):
     return player_x, player_y  # and return control to the player
 
 
+def moveEnemy(enemy_x, enemy_y, player_x, player_y, wave, action_timer):
+    x_dist = enemy_x - player_x
+    y_dist = enemy_y - player_y
+    tile_size = 40
+    if wave == 1:
+        if abs(x_dist) > tile_size * 3 // 5:
+            enemy_x += -1.5 if x_dist > 0 else 1.5
+
+        if abs(y_dist) > tile_size * 3 // 5:
+            enemy_y += -1.5 if y_dist > 0 else 1.5
+
+    elif wave == 2:
+        if abs(x_dist) > tile_size * 3 // 5:
+            enemy_x += -1.5 if x_dist > 0 else 1.5
+
+        if abs(y_dist) > tile_size * 3 // 5:
+            enemy_y += -1.5 if y_dist > 0 else 1.5
+
+        if action_timer**2 > random.randint(60**2, 300**2):
+            action_timer = 0  # fire an arrow
+
+    elif wave == 3:
+        if action_timer < 600:
+            if abs(x_dist) > tile_size * 3 // 5:
+                enemy_x += -1 if x_dist > 0 else 1
+
+            if abs(y_dist) > tile_size * 3 // 5:
+                enemy_y += -1 if y_dist > 0 else 1
+        elif action_timer == 600:
+            enemy_x = random.randint(tile_size * 3, tile_size * 11)
+            enemy_y = random.randint(tile_size * 3, tile_size * 11)
+        elif action_timer == 900:
+            action_timer = 0
+
+    elif wave == 4:
+        if action_timer == 0:
+            action_timer = 600
+        if action_timer < 600:
+            if abs(x_dist) > tile_size * 3 // 5:
+                enemy_x += -1 if x_dist > 0 else 1
+
+            if abs(y_dist) > tile_size * 3 // 5:
+                enemy_y += -1 if y_dist > 0 else 1
+        else:
+            if abs(x_dist) > tile_size * 3 // 5:
+                enemy_x += -1.5 if x_dist > 0 else 1.5
+
+            if abs(y_dist) > tile_size * 3 // 5:
+                enemy_y += -1.5 if y_dist > 0 else 1.5
+
+    elif wave == 5:
+        if action_timer <= 300:
+            enemy_y -= 1
+        else:
+            enemy_y += 1
+            if action_timer == 600:
+                action_timer = 0
+        if random.randint(1, 300) == 1:
+            pass  # spawn a laser
+        pass  # demilich
+    action_timer += 1
+    return enemy_x, enemy_y, action_timer
+
+
 def main():
     # make a pygame window updating at 60 FPS
     pygame.init()
@@ -109,6 +173,7 @@ def main():
     magic_type = -1
     magic_x, magic_y = 0, 0
     magic_collected = False
+    magic_positions = []
 
     # set attributes for aster's arrow
     fire_timer = 0
@@ -123,12 +188,13 @@ def main():
     wave = 1
     wave_length = wave_totals[wave]
     wave_done = False
-    health_pools = [0, 1, 2, 5, 10, 20]
+    health_pools = [0, 1, 2, 5, 10, 30]
 
     # "constants" for enemies
     ENEMY_X = 0
     ENEMY_Y = 1
     HP = 2
+    SPECIAL_TIMER = 3
 
     while not done:
         screen.fill((0, 0, 0))  # clear the screen
@@ -137,7 +203,7 @@ def main():
         screen.blit(img, (0, 0))  # blits start at the top corner
 
         spawn_timer += 1
-        if spawns < wave_length and (spawn_timer == 75 + (wave * 10) or not random.randint(0, 300)):  # spawn an enemy
+        if spawns < wave_length and (spawn_timer == 60 + (wave * 40) or not random.randint(0, 600)):  # spawn an enemy
             spawn_points = [  # each line holds the data for a wave, each item holds a spawn point
                 [],
                 [[tile_size, tile_size * 3], [tile_size * 2, tile_size * 13], [tile_size * 14, tile_size * 7]],
@@ -149,50 +215,57 @@ def main():
             new_spawn = spawn_points[wave]  # get the spawn points for the wave
             new_spawn = new_spawn[random.randint(0, len(new_spawn) - 1)]  # and pick a random one from them
             new_spawn.append(health_pools[wave])
+            new_spawn.append(0)
             living_enemies.append(new_spawn)
             spawn_timer = 0
             spawns += 1
 
+        magic_positions.append(arrow_position)
         for enemy in living_enemies:
             img = pygame.image.load("blue.jpg")
             img = pygame.transform.scale(img, (tile_size, tile_size))
+
+            # make the enemy move - will move into a function to refactor
+            enemy[ENEMY_X], enemy[ENEMY_Y], enemy[SPECIAL_TIMER] = moveEnemy(enemy[ENEMY_X], enemy[ENEMY_Y], player_x, player_y, wave, enemy[SPECIAL_TIMER])
+
             x_dist = enemy[ENEMY_X] - player_x
             y_dist = enemy[ENEMY_Y] - player_y
 
-            # make the enemy move - will move into a function to refactor
-            if abs(x_dist) > tile_size * 3//5:
-                enemy[ENEMY_X] += -1.5 if x_dist > 0 else 1.5
-
-            if abs(y_dist) > tile_size * 3//5:
-                enemy[ENEMY_Y] += -1.5 if y_dist > 0 else 1.5
-
-            # if the player and enemy are colliding, damage the player and make aster immune for a short time
+            # if aster and this enemy are colliding, damage aster, knock him back, and make him immune for a short time
             if abs(x_dist) <= tile_size and abs(y_dist) <= tile_size and not immunity_frames:
-                player_hp -= 1
+                player_hp -= (wave + 1) // 2
                 immunity_frames = 75
                 done = (player_hp <= 0)
                 player_x += -25 if x_dist > 0 else 25
                 player_y += -25 if y_dist > 0 else 25
+                if wave == 4 and enemy[SPECIAL_TIMER] >= 1200:  # bugbear's special attack
+                    enemy[SPECIAL_TIMER] = 0
+                    player_hp -= 1
+                    enemy[HP] += 1
 
-            # if the arrow hits the enemy, damage the enemy and remove the arrow
-            if arrow_position[0] - tile_size <= enemy[ENEMY_X] <= arrow_position[0] + (tile_size // 2) and \
-                    arrow_position[1] - tile_size <= enemy[ENEMY_Y] <= arrow_position[1] + (tile_size // 2):
-                arrow_position = [0, 0]
-                enemy[HP] -= 1
-                if enemy[HP] <= 0:
-                    living_enemies.remove(enemy)
-                    wave_done = (spawns == wave_length and not living_enemies)  # if all the enemies have been killed
+            # if the arrow / magic hits the enemy, damage the enemy and remove the arrow / magic
+            for i in range(len(magic_positions)):
+                if magic_positions[i][0] - tile_size <= enemy[ENEMY_X] <= magic_positions[i][0] + (tile_size // 2) and \
+                        magic_positions[i][1] - tile_size <= enemy[ENEMY_Y] <= magic_positions[i][1] + (tile_size // 2):
+                    magic_positions[i] = [0, 0]
+                    if i+1 != len(magic_positions()):
+                        enemy[HP] -= 1
+                    enemy[HP] -= 1
+            if enemy[HP] <= 0:
+                living_enemies.remove(enemy)
+                wave_done = (spawns == wave_length and not living_enemies)  # if all the enemies have been killed
 
-                    # if the player doesn't have a power up, roll for a power up being spawned
-                    if not random.randint(0, 8 - wave) and not magic_collected:
-                        magic_type = random.randint(0, 12) // 2
-                        if magic_type > 4:  # make cure wounds appears twice as often as the damaging magic spells
-                            magic_type -= 1
-                        magic_x, magic_y = enemy[ENEMY_X], enemy[ENEMY_Y]
+                # if the player doesn't have a power up, roll for a power up being spawned
+                if not random.randint(0, 8 - wave) and not magic_collected:
+                    magic_type = random.randint(0, 12) // 2
+                    if magic_type > 4:  # make cure wounds appears twice as often as the damaging magic spells
+                        magic_type -= 1
+                    magic_x, magic_y = enemy[ENEMY_X], enemy[ENEMY_Y]
 
             else:
                 screen.blit(img, (int(enemy[ENEMY_X]), int(enemy[ENEMY_Y])))
-
+        arrow_position = magic_positions[len(magic_positions)-1]
+        magic_positions.pop(len(magic_positions)-1)
         if immunity_frames:
             immunity_frames -= 1
 
@@ -215,7 +288,7 @@ def main():
                         tile_size * 11 // 2 <= player_x <= tile_size * 17 // 2:  # move to the next map
                     player_x, player_y = waveTransition(player_x, player_y, wave, tile_size, backgrounds, screen, clock)
                     wave += 1
-                    wave_length += 5
+                    wave_length = wave_totals[wave]
                     spawns = 0
                     wave_done = False
             else:
@@ -292,11 +365,48 @@ def main():
                 img = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
                 screen.blit(img, (int(arrow_position[0]), int(arrow_position[1])))
 
-        if magic_collected:
+        if magic_collected and not magic_positions:
             if keys[pygame.K_SPACE]:
+                if magic_type == 4:
+                    player_hp += 2
+                    if player_hp > max_hp:
+                        player_hp = max_hp
+                elif magic_type == 5:
+                    for enemy in living_enemies:
+                        enemy[HP] -= 5
+                else:
+                    magic_positions = [[player_x + (tile_size // 4), player_y + (tile_size // 4)] for i in range(4)]
                 magic_type = -1
                 magic_collected = False
                 pass  # power ups will go here
+
+        elif magic_positions:
+            img = pygame.image.load("yellow.jpg")
+            img = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+
+            if magic_positions[0][0] <= 0:
+                magic_positions[0] = [0, 0]
+            else:
+                magic_positions[0][0] -= 10
+
+            if magic_positions[1][0] == 0 or magic_positions[1][0] > display_width:
+                magic_positions[1] = [0, 0]
+            else:
+                magic_positions[1][0] += 10
+
+            if magic_positions[2][1] <= 0:
+                magic_positions[2] = [0, 0]
+            else:
+                magic_positions[2][1] -= 10
+
+            if magic_positions[3][1] == 0 or magic_positions[3][1] > display_height:
+                magic_positions[3] = [0, 0]
+            else:
+                magic_positions[3][1] += 10
+
+            for i in range(4):
+                if magic_positions[i][1]:
+                    screen.blit(img, (int(magic_positions[i][0]), int(magic_positions[i][1])))
 
         # update at end of frame
         pygame.display.flip()
